af8a0b2a8657d079d8588b326fa8586a75212947
reagul <reagul.2007@gmail.com>
2018-02-05 20:57:00 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DOTNETCORE2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Started Console app");
+		int i; 
+	 i = 1; 	
+            while (true) 
+ 		{
+                Console.WriteLine("Loop # {0}", i);
+		System.Threading.Thread.Sleep(300000);
+                i++;
+            }
+ 
+        }
+
+    }
+}
